ca9f41413b73e3a86dd563bc6351fc27487d2aa5
minh <Lminhphu0304@gmail.com>
2020-03-10 17:49:38 +1300

    Add browser router from react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core'
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import { Home } from './pages/Home'
+import { Board } from './pages/Board'
+import './App.css'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#fcaf09',
+    },
+    secondary: {
+      main: '#000000'
+    }
+  },
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/board">
+            <Board />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
