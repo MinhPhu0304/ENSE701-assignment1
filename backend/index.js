b7879cbe63ab5e4d945aabcea758294df5342e88
minh <Lminhphu0304@gmail.com>
2020-03-14 12:56:21 +1300

    api enpoint to query Note in backend

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,19 @@
-require('dotenv').config()
-const express = require('express')
-const app = new express()
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true});
 
-app.get('/', (req, res) => {
+const app = new express();
+
+const { NoteModel } = require('./model/');
+
+app.get('/', async (req, res) => {
   return res.json({ message: 'ok'})
 })
 
+app.get('/api/note', async (req, res) => {
+  const result = await NoteModel.find()
+  return res.send({ result })
+})
 
-app.listen(process.env.PORT, () => console.log(`Server is listening on port ${process.env.PORT}`))
+app.listen(process.env.PORT, () => console.log(`Server is listening on port ${process.env.PORT}`));
