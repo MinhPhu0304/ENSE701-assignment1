95e4c2c38aab4a23a310773f81e94341a2c13e95
minh <Lminhphu0304@gmail.com>
2020-03-14 15:16:02 +1300

    Send index.html for root request

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.get('/api/note', async (req, res) => {
   return res.send({ result })
 })
 
-app.get('*', (req, res) => {  
+app.get('/', (req, res) => {  
   return res.sendFile('index.html', { root: path.join(__dirname, '../frontend/build')});
 })
 
