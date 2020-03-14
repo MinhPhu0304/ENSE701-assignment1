662d96c62dab2c66d09d3cb54a45ab2185aa2442
minh <Lminhphu0304@gmail.com>
2020-03-14 14:51:54 +1300

    Fix file path for express

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config({ path: '../.env'});
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true});
 
 const app = new express();
@@ -15,7 +16,7 @@ app.get('/api/note', async (req, res) => {
 })
 
 app.get('*', (req, res) => {  
-  return res.sendFile('index.html');
+  return res.sendFile('index.html', { root: path.join(__dirname, '../frontend/build')});
 })
 
 app.listen(process.env.PORT, () => console.log(`Server is listening on port ${process.env.PORT}`));
