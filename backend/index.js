5e28157d065b4d49c5df974018e5a9806da5fdc3
minh <Lminhphu0304@gmail.com>
2020-03-14 14:08:19 +1300

    Add hacky Heroku build command

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: '../.env'});
 const express = require('express');
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true});
