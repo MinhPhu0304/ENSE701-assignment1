b3106b54fac4dba78428cfa3abd689cf48dd4a9c
MinhPhu0304 <Lminhphu0304@gmail.com>
2020-03-15 09:33:04 +1300

    Change Board Content background color to white

--- frontend/src/pages/Board/Content.jsx
+++ frontend/src/pages/Board/Content.jsx
@@ -6,7 +6,7 @@ import './content.css'
 
 export function Content() {
   return (
-    <div style={{ backgroundColor: '#ffd400', height: '100%', width: '100%'}}>
+    <div style={{ backgroundColor: 'white', height: '100%', width: '100%'}}>
       <Note content="This is a test content This is a test content This is a test content This is a test content" />
       <div className="fab-container">
         <Fab color="secondary" aria-label="add">
