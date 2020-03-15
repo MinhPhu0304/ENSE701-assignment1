b5ea16c9e10921e1985f40b213d36b2dbacc0893
minh <Lminhphu0304@gmail.com>
2020-03-16 09:51:29 +1300

    Add redux store to root app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import { createMuiTheme, ThemeProvider } from '@material-ui/core'
 import React from 'react'
+import { Provider } from 'react-redux'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import { Home } from './pages/Home'
 import { Board } from './pages/Board'
+
+import { store } from './store'
 
 const theme = createMuiTheme({
   palette: {
@@ -18,17 +21,19 @@ const theme = createMuiTheme({
 
 export function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/board">
-            <Board />
-          </Route>
-        </Switch>
-      </Router>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/board">
+              <Board />
+            </Route>
+          </Switch>
+        </Router>
+      </ThemeProvider>
+    </Provider>
   )
 }
