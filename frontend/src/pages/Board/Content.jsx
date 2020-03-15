78f4426859985410306155ed0ca35dd2e9542a70
minh <Lminhphu0304@gmail.com>
2020-03-16 11:02:41 +1300

    Connect board content components to redux store

--- frontend/src/pages/Board/Content.jsx
+++ frontend/src/pages/Board/Content.jsx
@@ -1,13 +1,25 @@
 import { Fab } from '@material-ui/core'
 import React from 'react'
+import { connect } from 'react-redux'
 
 import { Note } from '../../atoms/Note'
 import './content.css'
 
-export function Content() {
+export const Content = connect(mapStateToProps)(ContentDump)
+
+function mapStateToProps (state) {
+  const { notes } = state
+  return {
+    notes
+  }
+}
+
+function ContentDump({ notes }) {
   return (
     <div style={{ backgroundColor: 'white', height: '100%', width: '100%'}}>
-      <Note content="This is a test content This is a test content This is a test content This is a test content" />
+      {
+        notes.map((note, key) => <Note key={key} content={note}/>)
+      }
       <div className="fab-container">
         <Fab color="secondary" aria-label="add">
           <i className="material-icons">add</i>
