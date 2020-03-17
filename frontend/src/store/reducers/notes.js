edd53b225e3755c002290fbc98076238356518c0
minh <Lminhphu0304@gmail.com>
2020-03-18 12:06:24 +1300

    Add upsert function for notes reducers

--- frontend/src/store/reducers/notes.js
+++ frontend/src/store/reducers/notes.js
@@ -1,18 +1,21 @@
+import { remove } from 'lodash'
+
 import { ADD_NOTE, ADD_NOTES, REMOVE_NOTES, UPSERT_NOTES } from '../actions/notes'
 
 const inititalState = []
 
 export function noteReducers(state = inititalState, action) {
   switch (action.type) {
+    case UPSERT_NOTES:
+      const { payload } = action
+      const removed = remove(state, (note) => note._id === payload._id)
+      return [ ...removed, payload]
     case ADD_NOTE:
       return [...state, action.payload]
     case ADD_NOTES:
       return [...state, ...action.payload]
     case REMOVE_NOTES:
-      return {
-        ...state,
-        note: state.notes.filter((note) => note !== action.payload)
-      }
+      return state.filter((note) => note !== action.payload)
     default: 
       return state
   }
