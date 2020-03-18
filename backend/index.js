ceae597873031ae5f4034afd755590271cc618da
minh <Lminhphu0304@gmail.com>
2020-03-19 12:10:23 +1300

    Lint fix

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
-require('dotenv').config({ path: '../.env'});
+require('dotenv').config({ path: '../.env' });
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true});
-const { NoteModel } = require('./model/');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const { NoteModel } = require('./model');
 
 const app = new express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,27 +14,25 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api/note', async (req, res) => {
-  const result = await NoteModel.find()
-  return res.send({ result })
-})
+  const result = await NoteModel.find();
+  return res.send({ result });
+});
 
 app.post('/api/note', async (req, res) => {
-  const { content } = req.body
+  const { content } = req.body;
   const newNote = new NoteModel({
-    content
-  })
-  await newNote.save()
-  return res.send({ newNote })
-})
+    content,
+  });
+  await newNote.save();
+  return res.send({ newNote });
+});
 
 app.put('/api/note', async (req, res) => {
-  const { content, _id } = req.body
-  const updatedNote = NoteModel.findOneAndUpdate({ _id }, { content })
-  return res.send({ updatedNote })
-})
+  const { content, _id } = req.body;
+  const updatedNote = NoteModel.findOneAndUpdate({ _id }, { content });
+  return res.send({ updatedNote });
+});
 
-app.get('/*', (req, res) => {  
-  return res.sendFile(path.join(__dirname, 'public', 'index.html'));
-})
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 
 app.listen(process.env.PORT, () => console.log(`Server is listening on port ${process.env.PORT}`));
