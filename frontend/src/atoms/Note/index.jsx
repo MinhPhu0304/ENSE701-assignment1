ef8cb4e2dffceedfb5b7019b277fafdf943b3bec
minh <Lminhphu0304@gmail.com>
2020-03-19 12:23:27 +1300

    Add api call for POST and PUT note

--- frontend/src/atoms/Note/index.jsx
+++ frontend/src/atoms/Note/index.jsx
@@ -2,6 +2,8 @@ import { Button, Typography, TextField } from '@material-ui/core'
 import React, { useState } from 'react'
 import { Rnd } from 'react-rnd'
 
+import { dispatch, Notes } from '../../store'
+
 import './styles.css'
 
 export function Note({ content }) {
@@ -9,13 +11,12 @@ export function Note({ content }) {
   const [note, setNote] = useState(content)
   const defaultSetting = getDefaultRNDSetting()
   const toggleEdit = () => setEditting(!isEditing)
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    // TODO: API request here
-    
     setEditting(!isEditing)
+    submitNote(note)
   }
-  const handleTextChange = (e) => setNote(e.target.value)
+  const handleTextChange = (e) => {setNote({...note, content: e.target.value}) }
   if(isEditing) {
     return (
       <Rnd className="Draggable" cancel="#note-input" default={defaultSetting} minWidth={defaultSetting.width} minHeight={defaultSetting.height} bounds="parent">
@@ -45,4 +46,33 @@ function getDefaultRNDSetting () {
     width: 210,
     height: 150,
   }
+}
+
+async function submitNote (note) {
+  if(note._id){
+    putNote(note)
+  }
+  postNewNote(note)
+}
+
+async function putNote (note) {
+  const body = await fetch(`${window.location.origin}/api/note`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ content: note.content, _id: note._id })
+  }).then(res => res.json())
+  dispatch(Notes.actions.upsertNote(body.newNote))
+}
+
+async function postNewNote (note) {
+  const body = await fetch(`${window.location.origin}/api/note`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ content: note.content })
+  }).then(res => res.json())
+  dispatch(Notes.actions.addNote(body.newNote))
 }
